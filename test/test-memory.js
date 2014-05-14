c7c2dcaf3b93ffd90658d1252b8960c687c3f957
Avery Brooks <averyjamesbrooks@gmail.com>
2014-05-14 12:08:42 -0400

    remove poetry / lyrics examples (file size and copyright)

--- test/test-memory.js
+++ test/test-memory.js
@@ -5,14 +5,8 @@ function dump(stack) {
 	console.log(stack.join(" ") + ".");
 }
 
-// console.log("loading cohen (synchronous)");
-// mdb.loadPoetry(mdb.loadFile("example/cohen.txt"));
-
-// console.log("loading dylan (synchronous)");
-// mdb.loadPoetry(mdb.loadFile("example/dylan.txt"));
-
 mdb.log("loading rimbaud (synchronous)");
-db.loadPoetry(mdb.loadFile("example/rimbaud.txt"));
+db.loadPoetry(mdb.loadFile("example/lorem.txt"));
 
 mdb.log("generating (asynchronous)");
 
